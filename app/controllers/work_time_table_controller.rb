dbed78510f283068b6c346b3da09c211989189b1
tom-mats <dawn.gray.sky@gmail.com>
2017-03-19 18:03:11 +0900

    init release

--- app/controllers/work_time_table_controller.rb
+++ app/controllers/work_time_table_controller.rb
@@ -14,7 +14,7 @@ class WorkTimeTableController < ApplicationController
       @activities << activity.to_s
       time_log_activity = @time_log[activity.to_s]
       TimeEntry.where("activity_id = :id AND user_id = :user AND spent_on >= :from AND spent_on <= :to", {:id => activity.id, :user => User.current, :from => from_date, :to => end_date }).each do |obj|
-        k = obj.spent_on.to_s
+        k = obj.spent_on.strftime(DATE_FMT)
         if time_log_activity.has_key?(k)
           time_log_activity[k] += obj.hours
         else
