63c011f6da4dce54274f24c11e6b14d9d5670369
tom-mats <dawn.gray.sky@gmail.com>
2017-03-19 17:55:45 +0900

    Support to get time entries

--- app/controllers/work_time_table_controller.rb
+++ app/controllers/work_time_table_controller.rb
@@ -5,6 +5,23 @@ class WorkTimeTableController < ApplicationController
   def index
     @user = User.current
     @month = Time.parse(params[:month] || Time.now.strftime(DATE_FMT))
+    from_date = @month.beginning_of_month.to_date
+    end_date = @month.end_of_month.to_date
+    @activities = []
+    @time_log = {}
+    TimeEntryActivity.all().each do |activity|
+      @time_log[activity.to_s] = {}
+      @activities << activity.to_s
+      time_log_activity = @time_log[activity.to_s]
+      TimeEntry.where("activity_id = :id AND user_id = :user AND spent_on >= :from AND spent_on <= :to", {:id => activity.id, :user => User.current, :from => from_date, :to => end_date }).each do |obj|
+        k = obj.spent_on.to_s
+        if time_log_activity.has_key?(k)
+          time_log_activity[k] += obj.hours
+        else
+          time_log_activity[k] = obj.hours
+        end
+      end
+    end
   end
 
   def next
