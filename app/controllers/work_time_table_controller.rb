d927a32b0f6547a0dc49156010498ae90847c25e
tom-mats <dawn.gray.sky@gmail.com>
2017-03-19 16:22:27 +0900

    Add table to show day in the specified month
    Remove debug msg

--- app/controllers/work_time_table_controller.rb
+++ app/controllers/work_time_table_controller.rb
@@ -1,12 +1,10 @@
 class WorkTimeTableController < ApplicationController
   unloadable
 
-
+  DATE_FMT = '%Y-%m-%d'
   def index
     @user = User.current
-    @month = Time.parse(params[:month] || Time.now.strftime('%Y-%m-%d'))
-    puts @month
-    puts @user
+    @month = Time.parse(params[:month] || Time.now.strftime(DATE_FMT))
   end
 
   def next
