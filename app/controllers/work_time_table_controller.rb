8fc6fdfbf2cec6ecd4f1f6cefaf4942bdd1d6e96
tom-mats <dawn.gray.sky@gmail.com>
2017-03-27 23:09:19 +0900

    Add link to time log details

--- app/controllers/work_time_table_controller.rb
+++ app/controllers/work_time_table_controller.rb
@@ -7,11 +7,11 @@ class WorkTimeTableController < ApplicationController
     @month = Time.parse(params[:month] || Time.now.strftime(DATE_FMT))
     from_date = @month.beginning_of_month.to_date
     end_date = @month.end_of_month.to_date
-    @activities = []
+    @activities = {}
     @time_log = {}
     (from_date..end_date).each { |e| @time_log[e.strftime(DATE_FMT)] = {}  }
     TimeEntryActivity.all().each do |activity|
-      @activities << activity.to_s
+      @activities[activity.id] = activity.to_s
       a = activity.to_s
       TimeEntry.where("activity_id = :id AND user_id = :user AND spent_on >= :from AND spent_on <= :to", {:id => activity.id, :user => User.current, :from => from_date, :to => end_date }).each do |obj|
         d = obj.spent_on.strftime(DATE_FMT)
